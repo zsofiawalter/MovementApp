7105f9efa4135701546d7b0dfba5495374d2714d
Danny Campbell <75504000+dcampbell3@users.noreply.github.com>
2024-07-05 04:17:21 -0400

    003 MVP (#1)
    First draft of MVP - timer that starts with focus countdown, alternates between focus and movement. User can start and stop the timer. Timer time can be modified within time_config.json file.

---
+++ frontend/src/TimerComponents/Timer.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import './timer_style.css'; // Import CSS for styling
+
+interface TimerProps {
+  minutes: number;
+  seconds: number;
+  title: string;
+  onComplete: () => void;
+}
+
+const Timer: React.FC<TimerProps> = ({ minutes, seconds, title, onComplete }) => {
+  const [timeLeft, setTimeLeft] = useState<number>(minutes * 60 + seconds);
+  const [timerRunning, setTimerRunning] = useState<boolean>(true);
+  const [timerComplete, setTimerComplete] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (timerRunning && timeLeft >= 0) {
+      const timer = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    } else if (timeLeft <= 0) {
+      setTimerComplete(true);
+      onComplete();
+    }
+  }, [timeLeft, timerRunning, onComplete]);
+
+  const displayMinutes: string = Math.floor(timeLeft / 60).toString().padStart(2, '0');
+  const displaySeconds: string = (timeLeft % 60).toString().padStart(2, '0');
+
+  return (
+    <div className="countdown-container">
+      <div className='title'>
+        <p className='title-text'>{title}</p>
+      </div>
+      {(
+        <div className="timer">
+          <p className="timer-text">{displayMinutes}:{displaySeconds}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Timer;
