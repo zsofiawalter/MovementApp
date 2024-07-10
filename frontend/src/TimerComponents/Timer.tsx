24d559892b618feb74485efdcee6c36cecf61ea9
zsofiawalter <zsofia.walter8@gmail.com>
2024-07-10 10:32:27 +0200

    Added warning notification

--- frontend/src/TimerComponents/Timer.tsx
+++ frontend/src/TimerComponents/Timer.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './timer_style.css'; // Import CSS for styling
+import React, { useState, useEffect } from "react";
+import "./timer_style.css"; // Import CSS for styling
+import addNotification from "react-push-notification";
 
 interface TimerProps {
   minutes: number;
@@ -8,13 +9,22 @@ interface TimerProps {
   onComplete: () => void;
 }
 
-const Timer: React.FC<TimerProps> = ({ minutes, seconds, title, onComplete }) => {
+const Timer: React.FC<TimerProps> = ({
+  minutes,
+  seconds,
+  title,
+  onComplete,
+}) => {
   const [timeLeft, setTimeLeft] = useState<number>(minutes * 60 + seconds);
   const [timerRunning, setTimerRunning] = useState<boolean>(true);
   const [timerComplete, setTimerComplete] = useState<boolean>(false);
 
   useEffect(() => {
     if (timerRunning && timeLeft >= 0) {
+      if (timerRunning && timeLeft === 60) {
+        warning();
+      }
+
       const timer = setTimeout(() => {
         setTimeLeft(timeLeft - 1);
       }, 1000);
@@ -26,19 +36,32 @@ const Timer: React.FC<TimerProps> = ({ minutes, seconds, title, onComplete }) =>
     }
   }, [timeLeft, timerRunning, onComplete]);
 
-  const displayMinutes: string = Math.floor(timeLeft / 60).toString().padStart(2, '0');
-  const displaySeconds: string = (timeLeft % 60).toString().padStart(2, '0');
+  const displayMinutes: string = Math.floor(timeLeft / 60)
+    .toString()
+    .padStart(2, "0");
+  const displaySeconds: string = (timeLeft % 60).toString().padStart(2, "0");
+
+  const warning = () => {
+    addNotification({
+      title: "1 minute left!",
+      message: "Time to wrap up",
+      duration: 10000,
+      native: true,
+    });
+  };
 
   return (
     <div className="countdown-container">
-      <div className='title'>
-        <p className='title-text'>{title}</p>
+      <div className="title">
+        <p className="title-text">{title}</p>
       </div>
-      {(
+      {
         <div className="timer">
-          <p className="timer-text">{displayMinutes}:{displaySeconds}</p>
+          <p className="timer-text">
+            {displayMinutes}:{displaySeconds}
+          </p>
         </div>
-      )}
+      }
     </div>
   );
 };
